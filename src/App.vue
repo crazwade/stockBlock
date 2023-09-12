adb32c93cee8dbcd364cebe98e16c4330a5e8936
jacky <jackywu526@gmail.com>
2023-09-12 12:05:35 +0800

    feat: 串接API

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,13 @@
     </template>
   </el-dialog>
   <div>
+    <select v-model="selectValue">
+      <option value="上市個股日本益比">上市個股日本益比</option>
+      <option value="上市個股殖利率">上市個股殖利率</option>
+      <option value="上市個股股價淨值比">上市個股股價淨值比</option>
+      <option value="每日收盤行情">每日收盤行情</option>
+      <option value="上市個股日成交資訊">上市個股日成交資訊</option>
+    </select>
     <el-button @click="fetchData">Fetch Data</el-button>
     <div class=" w-full">
       <div ref="barChartRef" id="chart" style="height: 600px;" class=" mx-auto" />
@@ -24,42 +31,85 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, getCurrentInstance } from 'vue';
-import { get_MI_INDEX } from './api/api';
-import type { BWIBBU_ALL_TYPE, MI_INDEX_TYPE } from './api/api';
+import { ref, onMounted } from 'vue';
+import { get_BWIBBU_ALL, get_MI_INDEX, get_STOCK_DAY_ALL, get_FMSRFK_ALL, get_FMNPTK_ALL } from './api/api';
+import * as echarts from 'echarts/core';
+import { CanvasRenderer } from 'echarts/renderers';
+import { TitleComponent } from 'echarts/components';
+import { TooltipComponent } from 'echarts/components';
+import { TreemapChart } from 'echarts/charts';
+
+type ApiFunctionKeys = '上市個股日本益比' | '上市個股殖利率' | '上市個股股價淨值比' | '每日收盤行情' | '上市個股日成交資訊';
 
 type dataType = {
-  指數: string;
-  收盤指數: string;
-  漲跌: string;
-  漲跌點數: string;
-  漲跌百分比: string;
-  特殊處理註記: string;
+  /** 名稱 */
+  name: string,
+  /** 比較的值 */
+  compare: string,
+  /** tooltip 內容 */
+  info: string,
 };
 
+type ApiFunction = () => Promise<dataType[]>;
+
+const apiFunctions: Record<ApiFunctionKeys, ApiFunction> = {
+  上市個股日本益比: get_BWIBBU_ALL,
+  上市個股殖利率: get_MI_INDEX,
+  上市個股股價淨值比: get_STOCK_DAY_ALL,
+  每日收盤行情: get_FMSRFK_ALL,
+  上市個股日成交資訊: get_FMNPTK_ALL,
+};
+
+echarts.use([CanvasRenderer, TitleComponent, TooltipComponent, TreemapChart]);
+
 // Initialize the echarts instance by passing in the DOM element
-let Chart: any;
+let Chart: echarts.ECharts;
 
 const dialogVisible = ref(false);
 
 const barChartRef = ref();
-const filteredData = ref<BWIBBU_ALL_TYPE[] | MI_INDEX_TYPE[]>([]);
-const target = ref('');
+const filteredData = ref<dataType[]>([]);
+const selectValue = ref('上市個股日本益比');
 
 const handleClose = () => {
   dialogVisible.value = false;
 };
 
-function setBarChartLayOut () {
+const fetchData = (async () => {
+  Chart.showLoading();
+  const apiFunction = apiFunctions[selectValue.value as ApiFunctionKeys];
+
+  if (!apiFunction) {
+    console.log(selectValue.value);
+    console.error('Invalid selection');
+    return;
+  }
+  // 可能會有多個不同的 API 也代表回傳的 RES 格式不同
+  const resp = await apiFunction();
+
+  if (resp.length === 0) {
+    // error
+  }
+
+  filteredData.value = resp;
+
   Chart.setOption({
     title: {
       // Set the chart title
-      text: '股票樹狀地圖',
+      text: `${selectValue.value} 地圖`,
+      top: 20,
     },
     series: [
       {
+        data: filteredData.value.map((item) => ({
+          name: item.name,
+          value: parseFloat(item.compare),
+          itemStyle: {
+            color: getColor(Number(item.compare)),
+          },
+        })),
         // Set the series name
-        name: 'Map',
+        name: selectValue.value,
         // Set the chart type to treemap
         type: 'treemap',
         levels: [
@@ -76,69 +126,33 @@ function setBarChartLayOut () {
         },
       },
     ],
-    tooltip: {},
-  });
-}
-
-const fetchData = (async () => {
-  // 可能會有多個不同的 API 也代表回傳的 RES 格式不同
-  const resp = await get_MI_INDEX('+');
-
-  if (resp.filteredData.length === 0) {
-    // error
-  }
-
-  filteredData.value = resp.filteredData;
-  target.value = resp.target;
-
-  Chart.setOption({
-    series: [
-      {
-        data: filteredData.value.map(item => ({
-          name: item.指數,
-          value: parseFloat(item.漲跌百分比),
-          itemStyle: {
-            color: getColor(Number(item.漲跌百分比)),
-          },
-        })),
-      },
-    ],
     tooltip: {
       // Set the tooltip format
       formatter: function (params: { name: string }) {
-      const item: dataType | undefined = filteredData.value.find(item => item.指數 === params.name);
+      const item = (filteredData.value as dataType[]).find(item => item.name === params.name);
 
       if (!item) {
         return '';
       }
 
-      return `指數: ${item.指數}<br>
-              收盤指數: ${item.收盤指數}<br>
-              漲跌: ${item.漲跌}<br>
-              漲跌點數: ${item.漲跌點數}<br>
-              漲跌百分比: ${item.漲跌百分比}<br>
-              特殊處理註記: ${item.特殊處理註記}`;
+      return item.info;
       }
     },
   });
+  Chart.hideLoading();
 });
 
 function getColor(num: number) {
-  const getMin = Math.min(...filteredData.value.map(item => parseFloat(item[target.value])));
-  const getMax = Math.max(...filteredData.value.map(item => parseFloat(item[target.value])));
+  const getMin = Math.min(...(filteredData.value as dataType[]).map(item => parseFloat(item.compare)));
+  const getMax = Math.max(...(filteredData.value as dataType[]).map(item => parseFloat(item.compare)));
   const lightness = (num - getMin) / (getMax - getMin);
   // 顏色深淺變化 80 最淺 / 60 最深
   return `hsl(0, 100%, ${80 - lightness * (80 - 60)}%)`;
 }
 
 onMounted (async () => {
-  const echarts = getCurrentInstance().appContext.config.globalProperties.$echarts;
-  Chart  = echarts.init(barChartRef.value);
-  setBarChartLayOut();
-  // Show a loading animation while the chart is being rendered
-  Chart.showLoading();
+  Chart = echarts.init(document.getElementById('chart'));
   await fetchData();
-  Chart.hideLoading();
 });
 </script>
 
@@ -148,3 +162,4 @@ onMounted (async () => {
   height: 100%;
 }
 </style>
+./api
